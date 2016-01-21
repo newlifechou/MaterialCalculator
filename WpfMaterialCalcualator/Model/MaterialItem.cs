68196ebd209ef9c602c442ce182cacbd46c3ae97
xs.zhou <newlifechou@outlook.com>
2016-01-21 15:35:33 +0800

    完成主窗口初步设计

---
+++ WpfMaterialCalcualator/Model/MaterialItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalcualator.Model
+{
+    /// <summary>
+    /// 实体类-材料库表项
+    /// </summary>
+    public class MaterialItem
+    {
+        public Guid Id { get; set; }
+        public string MaterialName { get; set; }
+        public double MoleWeight { get; set; }
+        public int PopRate { get; set; }
+
+
+
+    }
+}
