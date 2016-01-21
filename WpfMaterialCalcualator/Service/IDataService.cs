a78bde7031048f13a52b2f94c08027e277e6eb2b
xs.zhou <newlifechou@outlook.com>
2016-01-21 17:42:39 +0800

    初步开始MainViewModel

---
+++ WpfMaterialCalcualator/Service/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalcualator.Service
+{
+    /// <summary>
+    /// 数据服务接口
+    /// </summary>
+    public interface IDataService
+    {
+
+    }
+}
--- WpfMaterialCalcualator/ViewModel/MainViewModel.cs
+++ WpfMaterialCalcualator/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Collections.ObjectModel;
+using WpfMaterialCalcualator.Model;
 
 namespace WpfMaterialCalcualator.ViewModel
 {
@@ -16,6 +19,12 @@ namespace WpfMaterialCalcualator.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+
+        #region 私有成员区域
+
+        #endregion
+
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -30,5 +39,61 @@ namespace WpfMaterialCalcualator.ViewModel
             ////    // Code runs "for real"
             ////}
         }
+        #region 公共属性区域
+        //条件列表
+        private ObservableCollection<CalculationConditionItem> conditions;
+        public ObservableCollection<CalculationConditionItem> Conditions
+        {
+            get
+            {
+                return conditions;
+            }
+            set
+            {
+                Set(ref conditions, value);
+            }
+        }
+        //结果列表
+        private ObservableCollection<CalculationResultItem> results;
+        public ObservableCollection<CalculationResultItem> Results
+        {
+            get
+            {
+                return results;
+            }
+            set
+            {
+                Set(ref results, value);
+            }
+        }
+        //重量计算-计算组组名
+        private string calculationGroupName;
+        public string CalculationGroupName
+        {
+            get { return calculationGroupName; }
+            set
+            {
+                Set(ref calculationGroupName, value);
+            }
+        }
+        //重量计算-计算组重量
+        private double calculationWeight;
+        public double CalculationWeight
+        {
+            get { return calculationWeight; }
+            set
+            {
+                Set(ref calculationWeight, value);
+            }
+        }
+
+        #endregion
+
+        #region 公共命令区域
+        public RelayCommand AddMatertialCommand { get; private set; }
+        public RelayCommand CalculateCommand { get; private set; }
+        public RelayCommand ClearWeightCommand { get; private set; }
+        #endregion
+
     }
 }
