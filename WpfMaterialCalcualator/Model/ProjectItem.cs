52f5b4c315e4315152d5066031e5850cb6270531
xs.zhou <newlifechou@outlook.com>
2016-01-21 16:09:48 +0800

    添加LoadView和SaveView

---
+++ WpfMaterialCalcualator/Model/ProjectItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalcualator.Model
+{
+    /// <summary>
+    /// 实体类-项目名称表
+    /// </summary>
+    public class ProjectItem
+    {
+        public Guid ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime SaveDate { get; set; }
+    }
+}
