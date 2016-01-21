492de15df12c57f257a0073dc735a26a8a0084ac
xs.zhou <newlifechou@outlook.com>
2016-01-21 22:21:32 +0800

    初步创建sqlitehelper类

---
+++ WpfMaterialCalcualator/Service/IMaterialLibaryDataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMaterialCalcualator.Model;
+
+namespace WpfMaterialCalcualator.Service
+{
+    /// <summary>
+    /// 材料库数据服务
+    /// </summary>
+    public interface IMaterialLibaryDataService
+    {
+        IList<MaterialItem> GetAllMaterialItems();
+
+        bool AddMaterialItem(MaterialItem item);
+        bool UpdateMaterialItem(MaterialItem item);
+        bool DeleteMaterialItem(MaterialItem item);
+
+
+
+
+    }
+}
