8a7b1b25046e79d8a9faa84b66013f2977db96f6
xs.zhou <newlifechou@outlook.com>
2016-01-21 23:35:21 +0800

    添加空的ViewModel

---
+++ WpfMaterialCalcualator/ViewModel/MaterialLibraryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace WpfMaterialCalcualator.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MaterialLibraryViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MaterialLibraryViewModel class.
+        /// </summary>
+        public MaterialLibraryViewModel()
+        {
+        }
+    }
+}
