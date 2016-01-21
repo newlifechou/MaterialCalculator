0f46c4d19ea5c8438ad281732519403f795b182f
xs.zhou <newlifechou@outlook.com>
2016-01-21 15:44:20 +0800

    添加两个Model类

---
+++ WpfMaterialCalcualator/Model/CalculationConditionItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalcualator.Model
+{
+    /// <summary>
+    /// 实体类-计算条件表项目
+    /// </summary>
+    public class CalculationConditionItem
+    {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public double MoleWeight { get; set; }
+        public double At { get; set; }
+    }
+}
---
+++ WpfMaterialCalcualator/Model/CalculationResultItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalcualator.Model
+{
+    /// <summary>
+    /// 实体类-计算结果表项目
+    /// </summary>
+    public class CalculationResultItem
+    {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public string GroupComposition { get; set; }
+        public double Tmp { get; set; }
+        public double Wt { get; set; }
+        public double Weight { get; set; }
+    }
+}
