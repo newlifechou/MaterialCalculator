de011756b808a887f38a947ea51e10b9666c6702
xs.zhou <newlifechou@outlook.com>
2016-01-23 16:27:38 +0800

    初步完成EditMaterialLibrary窗口

--- WpfMaterialCalcualator/ViewModel/MaterialLibraryViewModel.cs
+++ WpfMaterialCalcualator/ViewModel/MaterialLibraryViewModel.cs
@@ -1,4 +1,9 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using WpfMaterialCalcualator.Service;
+using WpfMaterialCalcualator.Model;
+using System.Collections.ObjectModel;
+using System;
 
 namespace WpfMaterialCalcualator.ViewModel
 {
@@ -10,11 +15,106 @@ namespace WpfMaterialCalcualator.ViewModel
     /// </summary>
     public class MaterialLibraryViewModel : ViewModelBase
     {
+        #region 私有变量
+        private readonly IMaterialLibraryDataService materialDataService;
+        private readonly IDialogService dialogService;
+        #endregion
         /// <summary>
         /// Initializes a new instance of the MaterialLibraryViewModel class.
         /// </summary>
-        public MaterialLibraryViewModel()
+        public MaterialLibraryViewModel(IMaterialLibraryDataService datads, IDialogService dialogds)
         {
+            materialDataService = datads;
+            dialogService = dialogds;
+            Reload();
+
+            AddCommand = new RelayCommand(AddAction);
+            EditCommand = new RelayCommand<MaterialItem>(EditAction);
+            DeleteCommand = new RelayCommand<MaterialItem>(DeleteAction);
+            SaveCommand = new RelayCommand(SaveAction);
+
         }
+
+        private void DeleteAction(MaterialItem item)
+        {
+            if (dialogService.ShowDialog("Delete this material?","Delete"))
+            {
+                materialDataService.DeleteMaterialItem(item);
+            }
+        }
+
+        private void EditAction(MaterialItem item)
+        {
+            EditMaterialItem = item;
+        }
+
+        private void SaveAction()
+        {
+            if (EditMaterialItem.Id==Guid.Empty)
+            {
+                EditMaterialItem.Id = Guid.NewGuid();
+                materialDataService.AddMaterialItem(EditMaterialItem);
+            }
+            else
+            {
+                materialDataService.UpdateMaterialItem(EditMaterialItem);
+            }
+            Reload();
+
+            EditMaterialItem.Id = Guid.Empty;
+            EditMaterialItem.MaterialName = string.Empty;
+            EditMaterialItem.MoleWeight = 0;
+            EditMaterialItem.PopRate = 0;
+            RaisePropertyChanged("EditMaterialItem");
+        }
+
+        private void AddAction()
+        {
+            EditMaterialItem = new MaterialItem();
+        }
+
+        private void Reload()
+        {
+            Materials = new ObservableCollection<MaterialItem>(materialDataService.GetAllMaterialItems());
+
+        }
+
+
+        #region 公开属性
+        /// <summary>
+        ///当前编辑的材料项
+        /// </summary>
+        private MaterialItem editMaterialItem;
+        public MaterialItem EditMaterialItem
+        {
+            get { return editMaterialItem; }
+            set
+            {
+                Set(ref editMaterialItem, value);
+            }
+        }
+
+        /// <summary>
+        /// 所有材料列表
+        /// </summary>
+        private ObservableCollection<MaterialItem> materials;
+        public ObservableCollection<MaterialItem> Materials
+        {
+            get { return materials; }
+            set
+            {
+                Set(ref materials, value);
+            }
+        }
+
+
+        #endregion
+
+        #region 公开命令
+        public RelayCommand AddCommand { get; private set; }
+        public RelayCommand<MaterialItem> EditCommand { get; private set; }
+        public RelayCommand<MaterialItem> DeleteCommand { get; private set; }
+        public RelayCommand SaveCommand { get; private set; }
+        #endregion
     }
 }
