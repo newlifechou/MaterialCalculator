b9c1b19b4947c8a07404e744c128b0cd36172950
xs.zhou <newlifechou@outlook.com>
2016-01-26 10:26:23 +0800

    添加验证规则作为资源

---
+++ WpfMaterialCalcualator/Resource/MustNotBeNullRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfMaterialCalcualator.Resource
+{
+    /// <summary>
+    /// 验证不为空
+    /// </summary>
+    public class MustNotBeNullRule:ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty(value.ToString()))
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                return new ValidationResult(false, "Can not be Null or Empty");
+            }
+        }
+    }
+}
