060af5b31e5e2bddd8389c3697001c621699dc01
xs.zhou <newlifechou@outlook.com>
2016-01-27 10:58:12 +0800

    添加一个自定义检验类接口

---
+++ WpfMaterialCalcualator/Resource/ICustomValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalcualator.Resource
+{
+    /// <summary>
+    /// 自定义检验类接口
+    /// </summary>
+    public interface ICustomValidation
+    {
+        bool IsNotEmpty(string text);
+        bool IsPositiveNumber(string text);
+        bool IsEmail(string text);
+
+        bool IsRegexMatch(string text, string regexExpression);
+    }
+}
