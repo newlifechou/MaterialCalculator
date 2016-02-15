68d53f2b41dd65e66cc66bcf78ed65af6446362e
xs.zhou <newlifechou@outlook.com>
2016-02-15 09:58:06 +0800

    完成主界面的us-en语言

--- WpfMaterialCalculator/Properties/Settings.Designer.cs
+++ WpfMaterialCalculator/Properties/Settings.Designer.cs
@@ -22,5 +22,17 @@ namespace WpfMaterialCalculator.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("us-en")]
+        public string language {
+            get {
+                return ((string)(this["language"]));
+            }
+            set {
+                this["language"] = value;
+            }
+        }
     }
 }
