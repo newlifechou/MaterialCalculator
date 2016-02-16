620e1ddcf228a7dc64fc41bef069e824e379661e
xs.zhou <newlifechou@outlook.com>
2016-02-16 11:18:08 +0800

    创建验证设定时间的测试类，并通过单元测试

---
+++ SoftwareAuthorizeLib/Permisson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareAuthorizeLib
+{
+    /// <summary>
+    /// 软件许可类
+    /// </summary>
+    public class Permisson
+    {
+        /// <summary>
+        /// 检查当前时间是否超过设定的许可时间
+        /// </summary>
+        /// <param name="permitTime">设定的许可时间</param>
+        /// <returns></returns>
+        public bool CheckTime(DateTime permitTime)
+        {
+            return DateTime.Now < permitTime;
+        }
+    }
+}
---
+++ WpfMaterialCalculatorUnitTest/TestSoftwareAuthorizeLib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwareAuthorizeLib;
+
+namespace WpfMaterialCalculatorUnitTest
+{
+    [TestClass]
+    public class TestSoftwareAuthorizeLib
+    {
+        private Permisson tester;
+        public TestSoftwareAuthorizeLib()
+        {
+            tester = new Permisson();
+        }
+
+        [TestMethod]
+        public void TestCheckTimeOk()
+        {
+            Assert.IsTrue(tester.CheckTime(new DateTime(2016,2,17)));
+        }
+
+        [TestMethod]
+        public void TestCheckTimeNotOK()
+        {
+            Assert.IsFalse(tester.CheckTime(new DateTime(2016, 2, 16)));
+        }
+
+
+
+
+
+
+
+
+    }
+}
