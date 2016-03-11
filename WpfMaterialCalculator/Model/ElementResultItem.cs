c26cf6b5311c35c7c2f4346c2fdffb90564d75a7
xs.zhou <newlifechou@outlook.com>
2016-03-11 14:12:17 +0800

    add ElementResultItem model

---
+++ WpfMaterialCalculator/Model/ElementResultItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMaterialCalculator.Model
+{
+    /// <summary>
+    /// 用于单质计算项
+    /// </summary>
+    public class ElementResultItem:ModelBase
+    {
+        public Guid Id { get; set; }
+        public string ElementName { get; set; }
+        public double Weight { get; set; }
+    }
+}
