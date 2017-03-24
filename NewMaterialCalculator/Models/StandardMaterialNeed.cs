a8c9d7db4376303547bc1e5cf8806f18595fabd1
xs.zhou <newlifechou@outlook.com>
2017-03-24 12:50:25 +0800

    create main window

---
+++ NewMaterialCalculator/Models/StandardMaterialNeed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMaterialCalculator.Models
+{
+    public class StandardMaterialNeed
+    {
+        public Guid ID { get; set; }
+        public string Composition { get; set; }
+        public string Dimension { get; set; }
+        public double Weight { get; set; }
+        public string Remark { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ NewMaterialCalculator/Models/StandardMoldFill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMaterialCalculator.Models
+{
+    public class StandardMoldFill
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public double Diameter { get; set; }
+        public double Thickness { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
