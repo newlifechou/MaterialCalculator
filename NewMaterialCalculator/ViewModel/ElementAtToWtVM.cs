503cd6974eadfa375623b4f75dceecc8c05bea5d
xs.zhou <newlifechou@outlook.com>
2017-03-26 15:23:00 +0800

    finish elementview

--- NewMaterialCalculator/ViewModel/ElementAtToWtVM.cs
+++ NewMaterialCalculator/ViewModel/ElementAtToWtVM.cs
@@ -6,20 +6,44 @@ using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using NewMaterialCalculator.Models;
+using NewMaterialCalculator.BasicService;
+using System.Collections.ObjectModel;
 
 namespace NewMaterialCalculator.ViewModel
 {
-    public class ElementAtToWtVM
+    public class ElementAtToWtVM : ViewModelBase
     {
         public ElementAtToWtVM()
         {
 
         }
 
+        public ObservableCollection<DcBDElement> Elements { get; set; }
+
+        public ObservableCollection<ElementModel> InputElements { get; set; }
+        public ObservableCollection<ElementModel> StandardAtElements { get; set; }
+        public ObservableCollection<ElementModel> StandardWtElements { get; set; }
+
+        private double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; RaisePropertyChanged(nameof(Weight)); }
+        }
+
+        private ElementModel currentInputElement;
+
+        public ElementModel CurrentInputElement
+        {
+            get { return currentInputElement; }
+            set { currentInputElement = value; RaisePropertyChanged(nameof(CurrentInputElement)); }
+        }
 
 
 
-
-
+        public RelayCommand<DcBDElement> Add { get; set; }
+        public RelayCommand Save { get; set; }
+        public RelayCommand<ElementModel> Delete { get; set; }
     }
 }
