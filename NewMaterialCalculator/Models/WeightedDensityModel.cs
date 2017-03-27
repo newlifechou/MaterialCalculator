af76adce352337a09e64e667e899db6275fd7388
xs.zhou <newlifechou@outlook.com>
2017-03-27 17:05:35 +0800

    stop here and start again tomorrow

---
+++ NewMaterialCalculator/Models/WeightedDensityModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMaterialCalculator.Models
+{
+    public class WeightedDensityModel
+    {
+        public Guid ID { get; set; }
+        public string CompositionName { get; set; }
+        public double MolWeight { get; set; }
+        public double At { get; set; }
+        public double Wt { get; set; }
+        public double Density { get; set; }
+    }
+}
