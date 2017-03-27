3c4a9a54942ef4eaca9795fac07ddc2d09b25ca8
xs.zhou <newlifechou@outlook.com>
2017-03-27 16:39:55 +0800

    add weightedcalculation

---
+++ NewMaterialCalculator/ViewModel/WeightedDensityVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace NewMaterialCalculator.ViewModel
+{
+    public class WeightedDensityVM:ViewModelBase
+    {
+        public WeightedDensityVM()
+        {
+
+        }
+
+
+    }
+}
